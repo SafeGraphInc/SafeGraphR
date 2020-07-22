2d52e8671ebea39f32217dfb00548330f03f0b8c
NickCH-K <nhuntington-klein@fullerton.edu>
2020-07-22 01:59:22 -0700

    Make the expand JSON commands work with uneven JSONs

--- R/expand_json.R
+++ R/expand_json.R
@@ -4,11 +4,14 @@
 #'
 #' It expands that JSON column into long format, with one row per observation per value of the JSON column, and then collapses everything according to the set of grouping variables.
 #'
-#' @param dt data.table object  (or something that can be coerced to data.table)
+#' IMPORTANT: This function may throw an error if any aggregation is being done and the JSON vectors are not of equal length. However, that shouldn't be a problem for any SafeGraph file.
+#'
+#' @param dt data.table object (or something that can be coerced to data.table)
 #' @param expand String indicating the JSON column to be expanded.
 #' @param index String indicating the name of the new index column
 #' @param by Character vector indicating the variables to group by after expanding. Set to \code{NULL} to aggregate across all initial rows, or set to \code{FALSE} to not aggregate at all (this will also add an \code{initial_rowno} column showing the original row number).
-#' @param fun Function that takes a vector and returns a single value to use when collapsing to the \code{by} level.
+#' @param fast Assumes that all the JSON vectors are of equal length, and adds the values together rather than using whatever is in \code{fun}.
+#' @param fun Function that takes a vector and returns a single value to use when collapsing to the \code{by} level. Requires \code{fast = FALSE}.
 #' @param na.rm Ignore missing values of \code{expand}
 #' @param set_key Set the key of \code{dt} to \code{by}. Set to \code{FALSE} if you have already set the key or want it returned without key.
 #' @examples
@@ -25,7 +28,7 @@
 #' @export
 
 expand_integer_json <- function(dt, expand,
-                                index = 'index', by = NULL,
+                                index = 'index', by = NULL, fast = TRUE,
                                 fun = sum, na.rm = TRUE, set_key = TRUE) {
 
   if (!('data.table' %in% class(dt))) {
@@ -52,16 +55,46 @@ expand_integer_json <- function(dt, expand,
     }
   }
 
-  # Read with from JSON and add
-  exptext <- paste0(".(",expand,"= Reduce('+',purrr::map(",expand,",jsonlite::fromJSON)))")
+  if (fast) {
+    # Read with from JSON and add
+    exptext <- paste0(".(",expand,"= Reduce('+',purrr::map(",expand,",jsonlite::fromJSON)))")
 
-  dt <- dt[,
-     eval(parse(text=exptext)),
-     by=by]
+    dt <- dt[,
+             eval(parse(text=exptext)),
+             by=by]
 
-  indextext <- paste0(index,':= seq_len(.N)')
+    indextext <- paste0(index,':= seq_len(.N)')
 
-  dt[,eval(parse(text=indextext)),by=by]
+    dt[,eval(parse(text=indextext)),by=by]
+  } else {
+    if (by == 'initial_rowno') {
+      exptext <- paste0(".(",expand," = jsonlite::fromJSON(",expand,"), ", index, " = seq_len(.N))")
+
+      dt <- dt[,
+               eval(parse(text=exptext)),
+               by=initial_rowno]
+    } else {
+      dt[,initial_rowno := 1:nrow(dt)]
+
+      exptext <- paste0(".(",expand," = jsonlite::fromJSON(",expand,")", paste(paste0(",",by,"=",by),collapse=''),")")
+
+      dt <- dt[,
+               eval(parse(text=exptext)),
+               by=initial_rowno]
+
+      indextext <- paste0(index, ' := seq_len(.N)')
+
+      dt[,eval(parse(text = indextext)), by = initial_rowno]
+
+
+      sumtext <- paste0(".(",expand," = fun(",expand,"))")
+
+      dt <- dt[,
+               eval(parse(text=sumtext)),
+               by=c(index, by)]
+    }
+
+  }
 
   return(dt)
 }
@@ -71,15 +104,14 @@ expand_integer_json <- function(dt, expand,
 #'
 #' This function accepts a \code{data.table} along with a set of grouping variables and a character-format category-style JSON column (i.e. starts with curly brackets, not square).
 #'
-#' WARNING: currently does not work properly if different rows have different categories.
-#'
 #' It expands that JSON column into long format, with one row per observation per value of the JSON column, and then collapses everything according to the set of grouping variables.
 #'
 #' @param dt data.table object (or something that can be coerced to data.table)
 #' @param expand String indicating the JSON column to be expanded.
 #' @param index String indicating the name of the new index column
 #' @param by Character vector indicating the variables to group by after expanding. Set to \code{NULL} to aggregate across all initial rows, or set to \code{FALSE} to not aggregate at all (this will also add an \code{initial_rowno} column showing the original row number).
-#' @param fun Function that takes a vector and returns a single value to use when collapsing to the \code{by} level.
+#' @param fast Assumes that all the JSON vectors are of the exact same categories, and adds the values together rather than using whatever is in \code{fun}.
+#' @param fun Function that takes a vector and returns a single value to use when collapsing to the \code{by} level. Requires \code{fast = FALSE}.
 #' @param na.rm Ignore missing values of \code{expand}
 #' @param set_key Set the key of \code{dt} to \code{by}. Set to \code{FALSE} if you have already set the key or want it returned without key.
 #' @examples
@@ -95,7 +127,7 @@ expand_integer_json <- function(dt, expand,
 #' @export
 
 expand_cat_json <- function(dt, expand,
-                                index = 'index', by = NULL,
+                                index = 'index', by = NULL, fast = FALSE,
                                 fun = sum, na.rm = TRUE, set_key = TRUE) {
 
   if (!('data.table' %in% class(dt))) {
@@ -125,18 +157,46 @@ expand_cat_json <- function(dt, expand,
   # Strip the double quotes
   dt[,eval(parse(text='expand')) := stringr::str_replace_all(eval(parse(text=expand)), '\\"\\"','\\"')]
 
-  # Read with fromJSON and add
-  exptext <- paste0(".(", expand, "= Reduce('+',purrr::map(purrr::map(purrr::map(",
-                    expand, ",jsonlite::fromJSON),unlist),as.numeric)))")
+  if (fast) {
+    # Read with fromJSON and add
+    exptext <- paste0(".(", expand, "= Reduce('+',purrr::map(purrr::map(purrr::map(",
+                      expand, ",jsonlite::fromJSON),unlist),as.numeric)))")
 
-  # The names
-  catnames <- jsonlite::fromJSON(dt[[expand]][1]) %>% unlist() %>% names()
+    # The names
+    catnames <- jsonlite::fromJSON(dt[[expand]][1]) %>% unlist() %>% names()
 
-  dt <- dt[,
-           eval(parse(text=exptext)),
-           by=by]
-  dt[,index := catnames, by = by]
-  data.table::setnames(dt,'index',index)
+    dt <- dt[,
+             eval(parse(text=exptext)),
+             by=by]
+    dt[,index := catnames, by = by]
+    data.table::setnames(dt,'index',index)
+  } else {
+    if (by == 'initial_rowno') {
+      exptext <- paste0(".(",expand," = unlist(jsonlite::fromJSON(",expand,")), ",
+                        index, " = names(unlist(jsonlite::fromJSON(",expand,"))))")
+
+      dt <- dt[,
+               eval(parse(text=exptext)),
+               by=initial_rowno]
+    } else {
+      dt[,initial_rowno := 1:nrow(dt)]
+
+      exptext <- paste0(".(",expand," = as.numeric(unlist(jsonlite::fromJSON(",expand,"))), ",
+                        index, " = names(unlist(jsonlite::fromJSON(",expand,")))",
+                        paste(paste0(",",by,"=",by),collapse=''),")")
+
+      dt <- dt[,
+               eval(parse(text=exptext)),
+               by=initial_rowno]
+
+
+      sumtext <- paste0(".(",expand," = fun(",expand,"))")
+
+      dt <- dt[,
+               eval(parse(text=sumtext)),
+               by=c(index, by)]
+    }
+  }
 
   return(dt)
 }
